e4ab18a0d2842342fc305652c70a08ce3fea1219
ramaniruddha <162605119+ramaniruddha@users.noreply.github.com>
2024-03-08 15:39:33 +0530

    Add files via upload

---
+++ 3rdcode.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    while(1)
+    {
+
+
+    printf("enter he value to check the value is odd or even\n");
+    scanf("%d",&i);
+    if(i%2==0)
+    {
+        printf("number entered is even\n");
+    }
+    else
+    {
+        printf("numder entered is odd \n");
+    }
+    }
+}
