87a26ab6776baf0067212c37be61cb7171b786a4
ramaniruddha <162605119+ramaniruddha@users.noreply.github.com>
2024-03-10 17:12:20 +0530

    Add files via upload

---
+++ 29thcode.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+struct node *create_node()
+{
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    if (newnode == NULL)
+    {
+        printf("Memory not allocated\n");
+    }
+    else
+    {
+        printf("Enter data: ");
+        scanf("%d", &newnode->data);
+        newnode->link = newnode; // Circular link to itself
+    }
+    return newnode;
+}
+
+struct node *insert_front(struct node *head)
+{
+    struct node *newnode = create_node();
+    if (newnode != NULL)
+    {
+        if (head == NULL)
+        {
+            head = newnode;
+        }
+        else
+        {
+            struct node *cur = head;
+            while (cur->link != head)
+            {
+                cur = cur->link;
+            }
+            cur->link = newnode;
+            newnode->link = head;
+            head = newnode;
+        }
+    }
+    return head;
+}
+
+void display(struct node *head)
+{
+    struct node *cur = head;
+    if (head == NULL)
+    {
+        printf("Empty list\n");
+    }
+    else
+    {
+        do
+        {
+            printf("%d ", cur->data);
+            cur = cur->link;
+        } while (cur != head);
+        printf("\n");
+    }
+}
+
+struct node *delete_front_node(struct node *head)
+{
+    if (head == NULL)
+    {
+        printf("Nothing to delete\n");
+    }
+    else
+    {
+        struct node *cur = head;
+        while (cur->link != head)
+        {
+            cur = cur->link;
+        }
+        cur->link = head->link;
+        struct node *temp = head;
+        head = head->link;
+        free(temp);
+    }
+    return head;
+}
+
+int main()
+{
+    struct node *head = NULL;
+
+    // Insert nodes at the front
+    head = insert_front(head);
+    head = insert_front(head);
+    head = insert_front(head);
+
+    // Display the circular linked list
+    printf("Circular Linked List: ");
+    display(head);
+
+    // Delete the first node
+    head = delete_front_node(head);
+
+    // Display the circular linked list after deletion
+    printf("Circular Linked List after deletion: ");
+    display(head);
+
+    return 0;
+}
